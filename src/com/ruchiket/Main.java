083b4a228dda538301cb9da1b8fb577e611795f7
Ruchiket100 <RuchiketBorse1@gmail.com>
2022-08-22 13:52:39 +0530

    refacotr: by adding constants

--- src/com/ruchiket/Main.java
+++ src/com/ruchiket/Main.java
@@ -5,23 +5,31 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        final byte PERCENT = 100;
+        final byte MONTHS = 12;
 
-    // get PrincipalAmount, Interest Rate, Period(years)
         Scanner scanner = new Scanner(System.in);
-        // input for Principal Amount\
+
+        // input for Principal Amount
         System.out.print("Enter Principal Amount : ");
         int principalAmount = scanner.nextInt();
+
         // input for Interest Rate
         System.out.print("Enter Interest Rate on Principal Amount : ");
-        double interestRate = scanner.nextDouble()/1200; // for 1 month
+        float annualInterestRate = scanner.nextFloat(); // for 1 month
+        float monthlyInterest = annualInterestRate / PERCENT /MONTHS;
+
         // input for Period
         System.out.print("Enter Period in years : ");
-        int period = scanner.nextByte() * 12; // in months
+        byte period = scanner.nextByte() ;
+        int noOfMonths = period * MONTHS;
 
     // find Mortgage
-        double mortgage = principalAmount*(interestRate*Math.pow((1 + interestRate), period)/(Math.pow((1 + interestRate),period) - 1));
+        double mortgage = principalAmount
+                * (monthlyInterest*Math.pow((1 + monthlyInterest), noOfMonths)
+                / (Math.pow((1 + monthlyInterest),noOfMonths) - 1));
 
     // show Mortgage
-        System.out.println("Mortgage : " + Math.floor(mortgage));
+        System.out.println("Mortgage : " + Math.round(mortgage));
     }
 }
